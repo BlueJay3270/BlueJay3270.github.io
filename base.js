8f794c8599d8cb01faf90aa4d09d0e652aa795da
Kellan Clapp <bluejay3270@gmail.com>
2020-05-12 12:56:37 -0600

    Moved Finished website files to master.

---
+++ base.js
@@ -0,0 +1,17 @@
+//"Next Button links to the next page"
+function nextPage(curentPage) {
+  curentPage += 1;
+  //for when on Github Site
+  //window.location.href = "https://bluejay3270.github.io/mainpage(" + curentPage + ")";
+  //for testing
+  window.location.href = "mainpage(" + curentPage + ").html";
+}
+
+//"Previous" Button links to previus page
+function lastPage(curentPage) {
+  curentPage -= 1;
+  //for when on github site:
+  window.location.href = "https://bluejay3270.github.io/mainpage(" + curentPage + ")";
+  //for testing:
+  window.location.href = 'mainpage(' + curentPage + ').html';
+}
